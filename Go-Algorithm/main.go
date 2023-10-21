e3154f21775a456fc963af45702c395338d5f5b6
tronyaginaa <tronyaginaa@gmail.com>
2023-10-21 18:28:21 +0300

    correct console output

--- Go-Algorithm/main.go
+++ Go-Algorithm/main.go
@@ -27,8 +27,9 @@ func polynom(coef []float64, x float64) float64 {
 }
 
 type Task struct {
-	coefficients        []float64
-	eps, a, b, solution float64
+	coefficients []float64
+	eps, a, b    float64
+	solution     *float64
 }
 
 func (task *Task) getTask() {
@@ -51,38 +52,51 @@ func (task *Task) getTask() {
 	fmt.Scan(&degree)
 	task.coefficients = arr
 	task.eps = math.Pow10(-degree)
+	task.solution = nil
 }
 
 func (task *Task) theBisectionMethod() {
 	valA := polynom(task.coefficients, task.a)
 	valB := polynom(task.coefficients, task.b)
+	var solution float64
 	if valA*valB > 0 {
-		fmt.Print("Корни на указанном промежутке отсутсвуют.")
+		return
 	}
 	if valA == 0 {
-		task.solution = task.a
+		solution = task.a
+		return
 	} else if valB == 0 {
-		task.solution = task.b
-	}
-	var c, valC float64
-	a := task.a
-	b := task.b
-	for math.Abs(a-b) > task.eps {
-		c = (a + b) / 2
-		valC = polynom(task.coefficients, c)
-		if valA*valC < 0 {
-			b = c
-		} else {
-			a = c
-			valA = valC
+		solution = task.b
+	} else {
+		var c, valC float64
+		a := task.a
+		b := task.b
+		for math.Abs(a-b) > task.eps {
+			c = (a + b) / 2
+			valC = polynom(task.coefficients, c)
+			if valC == 0 {
+				break
+			}
+			if valA*valC < 0 {
+				b = c
+			} else {
+				a = c
+				valA = valC
+			}
 		}
+		solution = (a + b) / 2
 	}
-	task.solution = (a + b) / 2
+	task.solution = &solution
 }
 
 func main() {
 	task := Task{}
 	task.getTask()
 	task.theBisectionMethod()
-	fmt.Println("Корень полинома на указанном промежутке:", task.solution)
+	if task.solution != nil {
+		fmt.Println("Корень полинома на указанном промежутке:", *task.solution)
+	} else {
+		fmt.Println("Корни на указанном промежутке отсутсвуют.")
+	}
+
 }
